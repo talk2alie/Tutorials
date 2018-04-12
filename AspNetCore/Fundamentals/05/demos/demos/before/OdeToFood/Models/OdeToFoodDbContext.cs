5d6f19e1dd8b5c4ab1f4aee0806a261d019afa8f
Mohamed Alie Pussah, II <talk2alie@outlook.com>
2018-04-12 18:05:52 -0400

    Update asp.net core tutorial with database functionality

---
+++ AspNetCore/Fundamentals/05/demos/demos/before/OdeToFood/Models/OdeToFoodDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Models
+{
+    public class OdeToFoodDbContext : DbContext
+    {
+        public OdeToFoodDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
